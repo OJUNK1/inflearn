c61e8a7be87e8c73848376480d0f6794728560dd
OJUNK1 <kozwns@gmail.com>
2024-05-26 16:19:01 +0900

    Spring Core Principles - Basic Edition: Spring Container && Spring Bean && Singleton Container

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -5,7 +5,7 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
-import hello.core.member.MemoryMemberRepositry;
+import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
@@ -16,15 +16,18 @@ public class AppConfig {
 
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepositry();
+        System.out.println("call AppConfig.memberRepository");
+        return new MemoryMemberRepository();
     }
     @Bean
     public DiscountPolicy discountPolicy() {
