634d03a1cd4c47aa2e4e39e9f3f2608eb276646d
OJUNK1 <kozwns@gmail.com>
2024-07-28 13:20:33 +0900

    Java mid1 -  Date && Time

--- java/java-mid1/src/enumeration/test/AuthGradeMain2.java
+++ java/java-mid1/src/enumeration/test/AuthGradeMain2.java
@@ -9,7 +9,6 @@ public class AuthGradeMain2 {
         System.out.print("당신의 등급을 입력하세요[GUEST, LOGIN, ADMIN]: ");
         String grade = scanner.nextLine();
 
-
         AuthGrade authGrade = AuthGrade.valueOf(grade.toUpperCase());
         System.out.println("당신의 등급은 " + authGrade.getDescription() + "입니다. ");
 
