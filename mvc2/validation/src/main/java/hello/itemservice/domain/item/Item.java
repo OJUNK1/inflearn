b8f20ced6bfca3161185799525830f87cbccea40
OJUNK1 <kozwns@gmail.com>
2025-07-29 23:21:10 +0900

    [COMMIT]  - Bean Validation

--- mvc2/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ mvc2/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -3,11 +3,31 @@ package hello.itemservice.domain.item;
 import lombok.Data;
 
 @Data
+/**
+ * 그런데 실제 사용해보면 제약이 많고 복잡하다. 그리고 실무에서는 검증 기능이 해당 객체의 범위를 넘어서는 경우들도
+ * 종종 등장하는데, 그런 경우 대응이 어렵다.
+ * 따라서 오브젝트 오류(글로벌 오류)의 경우 `@ScriptAssert` 을 억지로 사용하는 것 보다는 다음과 같이 오브젝트 오
+ * 류 관련 부분만 직접 자바 코드로 작성하는 것을 권장한다.
+ */
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000원 넘게 입력해주세요.")
 public class Item {
 
+    /**
+     * 사실 groups 기능은 실제 잘 사용되지는 않는데, 그 이유는 실무에서는 주로 다음에 등장하는 등록용 폼 객체와 수정용
+     * 폼 객체를 분리해서 사용하기 때문이다.
+     */
+   // @NotNull(groups = UpdateCheck.class)
     private Long id;
+
+    //@NotBlank(message = "공백X", groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
+
+    //@NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    //@Range(min = 1000, max = 100000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
+
+    //@NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    //@Max(value = 9999, groups = {SaveCheck.class})
     private Integer quantity;
 
     public Item() {
